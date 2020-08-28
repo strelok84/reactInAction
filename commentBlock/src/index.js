a262bb5dc93e1d100c1de59939b6ddf4c10a18f1
Andrey Bespalov <streloc84@gmail.com>
2020-08-28 05:11:35 +0400

    str

--- commentBlock/src/index.js
+++ commentBlock/src/index.js
@@ -1,15 +1,75 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import PropTypes from "prop-types";
 
-const node = document.getElementById("root");
+const node=document.getElementById("root");
 
-const root = React.createElement("div",{}, //
-  React.createElement("h1",{},"Hello world!",//
-    React.createElement("a",{ href: "mailto:mark@ifelse.io" },
-      React.createElement("h1", {}, "React In Action"),
-      React.createElement("em", {}, "...and now it really is!")
+class Post extends Component{
+  render(){
+    return React.createElement(
+      "div",
+      {
+        className:"post"
+      },
+      React.createElement(
+        "h1",
+        {
+          className:"postAuthor",
+          id:this.props.id
+        },
+        this.props.user,
+        React.createElement(
+          "span",
+          {
+            className:"postBody"
+          },
+          this.props.content
+        ),
+        this.props.children
+      )
     )
-  )
+  }
+}
+
+class Comment extends Component{
+  render(){
+    return React.createElement(
+      "div",
+      {
+        className:"commentAuthor"
+      },
+      this.props.user,
+      React.createElement(
+        "span",
+        {
+          className:"commentContent"
+        },
+        this.props.content
+      )
+    )
+  }
+}
+Comment.PropTypes={
+  id:PropTypes.number.isRequired,
+  content:PropTypes.string.isRequired,
+  user:PropTypes.string.isRequired
+}
+Post.PropTypes={
+  user:PropTypes.string.isRequired,
+  content:PropTypes.string.isRequired,
+  id:PropTypes.number.isRequired
+};
+
+const App=React.createElement(Post,{
+  id:1,
+  content:"  said: This a post!",
+  user:"mark"
+},
+React.createElement(Comment,{
+  id:2,
+  user:"bob",
+  content:" commented: wow! how cool!"
+})
 );
 
-render(root, node);
+render (App, node);
